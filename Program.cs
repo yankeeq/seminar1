9ea5cb1b1b6780a0a1cc149b20eb716b394dd045
yankeeq <125729776+yankeeq@users.noreply.github.com>
2023-03-09 17:18:38 +0300

    открыл задачу

--- Program.cs
+++ Program.cs
@@ -84,7 +84,7 @@ else
 // Tesk 8: Напишите программу, которая на вход принимает 
 //число (N), а на выходе показывает все чётные числа от 1 до N.
 
-/*
+
 Console.WriteLine("Напишите число ");
 int n = Convert.ToInt32(Console.ReadLine());
 for(int i=1;i<=n;i++)
@@ -92,4 +92,3 @@ for(int i=1;i<=n;i++)
 if (i % 2 == 0)
 System.Console.Write(i + ", ");
 }
-*/
